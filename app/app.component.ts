94cd063d3cc7d7303ef9c1be299a65ae7d738f71
unknown <T.I@TI.rsatacadao.local>
2020-07-03 10:13:36 -0300

    alteração no arquivo app.components.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `<h2>Cadastro de Clientes</h2>
-    <clientes-lista></clientes-lista>
+    <router-outlet></router-outlet>
     `
 })
 export class AppComponent {}
