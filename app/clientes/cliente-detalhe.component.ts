838b4a5a79e359d5538ea42b76b46225c4205605
unknown <T.I@TI.rsatacadao.local>
2020-07-03 10:02:02 -0300

    criação do arquivo cliente-detalhe.component.html

---
+++ app/clientes/cliente-detalhe.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector : 'cliente-detalhe',
+    templateUrl : 'cliente-detalhe.component.html'
+})
+export class ClienteDetalheComponent {}
