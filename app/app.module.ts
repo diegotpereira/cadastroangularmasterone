474a677ce2f4d11da1e4daee9482bf20fe4f223a
unknown <T.I@TI.rsatacadao.local>
2020-07-03 09:49:33 -0300

    alteração no arquivo clientes.module movida para pasta clientes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ClientesModule } from './clientes/clientes.module'
+import { ClientesModule } from './clientes/clientes.module';
 @NgModule({
     imports : [BrowserModule, ClientesModule],
     declarations :[AppComponent],
