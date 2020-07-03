f9fe5921683f0363bfbd86583dfd40436ed9d336
unknown <T.I@TI.rsatacadao.local>
2020-07-03 09:37:10 -0300

    alteração no arquivo app.module.ts

--- app/app.module.js
+++ app/app.module.js
@@ -9,12 +9,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
+var clientes_module_1 = require("./clientes/clientes.module");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, clientes_module_1.ClientesModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         })
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,9 @@
-
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module'
 @NgModule({
-    imports : [BrowserModule],
+    imports : [BrowserModule, ClientesModule],
     declarations :[AppComponent],
     bootstrap: [AppComponent]
 })
