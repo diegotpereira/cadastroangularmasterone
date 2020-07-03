d9d9c3627ffa58057761ea49b4d3d033b3daf7ff
unknown <T.I@TI.rsatacadao.local>
2020-07-03 09:58:00 -0300

    alteração no arquivo clientes-lista.component.html

--- app/clientes/clientes-lista.component.ts
+++ app/clientes/clientes-lista.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
+import { CLIENTES } from './cliente.mock';
+import { Cliente } from './cliente.model';
+
 @Component({
     moduleId: module.id,
     selector : 'clientes-lista',
     templateUrl: 'clientes-lista.component.html'
 })
 export class ClientesListaComponent {
+
+    clientes : Cliente[] = CLIENTES ;
 }
